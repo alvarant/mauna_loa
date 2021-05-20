83d08a862ea5b6e5362e9438444ae751359ba253
Anton Alvarez <alvarant@oregonstate.edu>
2021-05-20 23:11:43 +0000

    first definition

--- ml.py
+++ ml.py
@@ -7,7 +7,8 @@ def get_df(filename):
     INPUT:filename e.g. mauna_loa.csv
     OUTPUT: Pandas Dataframe
     '''
-    
+    mauna_loa_df = pd.read_csv('mauna_loa.csv')
+    return mauna_loa_df
    
 
 def plot_df(df):
