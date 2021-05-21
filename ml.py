6a39f95c8aecde6207d635cc2bd5150534785e79
Anton Alvarez <alvarant@oregonstate.edu>
2021-05-21 00:04:47 +0000

    finished 2nd definition

--- ml.py
+++ ml.py
@@ -7,8 +7,8 @@ def get_df(filename):
     INPUT:filename e.g. mauna_loa.csv
     OUTPUT: Pandas Dataframe
     '''
-    mauna_loa_df = pd.read_csv('mauna_loa.csv')
-    return mauna_loa_df
+    df = pd.read_csv('mauna_loa.csv')
+    return df
    
 
 def plot_df(df):
@@ -16,5 +16,12 @@ def plot_df(df):
     INPUT: Pandas DataFrame
     OUTPUT: handle to plot axis
     '''
-    
+    years = df['decimal_date'][0] - df['decimal_date']
+    plt.ylabel('CO2 Levels')
+    plt.xlabel('Years since 1958')
+    plt.title('Rising CO2 Levels since 1958 Mauna Loa Observatory')
+    ax = plt.gca()
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    return plt.plot(years,df.C02)
     
